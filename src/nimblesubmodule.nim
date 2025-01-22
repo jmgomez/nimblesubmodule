20134b6f0fb3346e7b9884e603a9ed3307ff4b9c
jmgomez <info@jmgomez.me>
2025-01-22 13:55:20 +0000

    adds submodule

--- src/nimblesubmodule.nim
+++ src/nimblesubmodule.nim
@@ -1,5 +1,7 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
+import ../sat/src/sat/satvars
 
+let dontCare = DontCare
 when isMainModule:
   echo("Hello, World!")
